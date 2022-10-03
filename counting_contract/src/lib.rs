61e3e730319d9e917a41fd4f00339745863d0e7a
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-10-03 12:47:31 -0400

    lesson 3 complete

--- counting_contract/src/lib.rs
+++ counting_contract/src/lib.rs
@@ -1,4 +1,9 @@
-use cosmwasm_std::{entry_point, DepsMut, Empty, Env, MessageInfo, Response, StdError};
+use cosmwasm_std::{entry_point, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdError, StdResult, Binary, to_binary};
+use crate::msg::QueryMsg;
+use crate::msg::QueryMsg::{Value};
+
+mod contract;
+pub mod msg;
 
 #[entry_point]
 pub fn instantiate(
@@ -8,4 +13,15 @@ pub fn instantiate(
     _msg: Empty,
 ) -> Result<Response, StdError> {
     Ok(Response::new())
+}
+
+#[entry_point]
+pub fn query(
+    _deps: Deps,
+    _env: Env,
+    msg: QueryMsg,
+) -> StdResult<Binary> {
+    match msg {
+        Value {} => to_binary(&contract::query::value()),
+    }
 }
---
+++ counting_contract/src/msg.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    Value {},
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub struct ValueResp {
+    pub value: u64,
+}
