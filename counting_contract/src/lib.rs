fb42f2a8538596eda65f8ff276da55f6372c9e74
tacolopo <106569305+tacolopo@users.noreply.github.com>
2022-10-03 12:10:55 -0400

    lesson 2 complete

--- counting_contract/src/lib.rs
+++ counting_contract/src/lib.rs
@@ -1,1 +1,11 @@
-fn instantiate() {}
+use cosmwasm_std::{entry_point, DepsMut, Empty, Env, MessageInfo, Response, StdError};
+
+#[entry_point]
+pub fn instantiate(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: Empty,
+) -> Result<Response, StdError> {
+    Ok(Response::new())
+}
